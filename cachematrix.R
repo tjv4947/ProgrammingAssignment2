05c6846363b25a271154b6811d6b50c30ebce847
tjv4947 <tjv4947@yahoo.com>
2014-05-25 10:42:02 -0600

    Completed Code for Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These routines creates a matrix which can cache its inverse its inverse to 
+##     avoid re-computing for a matrix which has already been calculated.
 
-## Write a short comment describing this function
-
+##  Routine to create a cache for input matrix x and results using the super assignment '<<-' operator
+##  N.B. There is no error checking to make sure the matrix is properly sized, which could be added.
 makeCacheMatrix <- function(x = matrix()) {
+        invMatrix  <- NULL
+        set  <- function(y){
+                x <<- y
+                invMatrix <<- NULL 
+        }
+        get  <- function() x
+        setInv  <- function(solve) invMatrix  <<- solve
+        getInv  <- function() invMatrix
+        list(set= set, get = get, 
+             setInv = setInv, 
+             getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
-
+## Routine computes the inverse of the input matrix. If the inverse has already been calculated (and is unchanged),
+## then the cacheSolve retrieves the inverse from the cache and does not recalculate inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invMatrix  <- x$getInv()
+        if (!is.null(invMatrix)){
+                message("Getting data from cache")
+                return(invMatrix)
+        }
+        data  <- x$get()
+        invMatrix  <- solve(data, ...)
+        x$setInv(invMatrix)
+        invMatrix
 }
